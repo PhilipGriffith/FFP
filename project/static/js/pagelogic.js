59a08fcc07112574b45e6187fbc192f00a37b483
PhilipGriffith <philip.griffith@gmail.com>
2016-11-10 13:14:19 -0500

    Added table. Still need to fix gauge: it runs one move behind the user input.

--- project/static/js/pagelogic.js
+++ project/static/js/pagelogic.js
@@ -1,11 +1,13 @@
-//MongoDB URL
+//Set URLs
 var mongodb = "http://localhost:5000/query="
 var csv_data = "../static/data/ffp.csv"
 var geojson_data = "../static/geojson/florida.json"
 
+//Create cr number
 var cr = "0.0000";
 document.getElementById("cr-number").innerHTML = cr;
 
+//Create cr gauge
 var crGauge = Gauge('#cr-gauge', {
     size: 300,
     clipWidth: 300,
@@ -16,31 +18,30 @@ var crGauge = Gauge('#cr-gauge', {
 });
 crGauge.render();
 
-//Map width and height
+//Define margins
 var margin = {top: 20, right: 40, bottom: 20, left: 40};
+
+//Define map widths and heights
 var map_width = 700;
 var map_height = 580 - margin.top - margin.bottom;
 
 //Define map projection
 var projection = d3.geo.albers()
-                        .scale(4000)
-                        .rotate([83.5, -.6, 0])
-                        .center([0, 27])
-                        .translate([map_width/2, map_height/2]);
+   .scale(4000)
+   .rotate([83.5, -.6, 0])
+   .center([0, 27])
+   .translate([map_width/2, map_height/2]);
 
-//Define path generator
+//Define map path generator
 var path = d3.geo.path()
-                 .projection(projection);
+     .projection(projection);
 
 //Create map element
 var map = d3.select("body")
-            .append("svg")
-            .attr("id", "map")
-            .attr("width", width)
-            .attr("height", height);
-
-//Create bar chart element
-
+    .append("svg")
+    .attr("id", "map")
+    .attr("width", map_width + margin.right + margin.left)
+    .attr("height", map_height + margin.top + margin.bottom);
 
 //Load in GeoJSON data
 d3.json(geojson_data, function(json) {
@@ -55,24 +56,43 @@ d3.json(geojson_data, function(json) {
     // Load FFP data
     d3.csv(csv_data, function(csv) {
 
+        //Create map
         map.selectAll("circle")
             .data(csv)
             .enter()
             .append("circle")
             .attr("cx", function(d) {
-                    return projection([d.LON, d.LAT])[0];
+                return projection([d.LON, d.LAT])[0];
             })
             .attr("cy", function(d) {
-                    return projection([d.LON, d.LAT])[1];
+                return projection([d.LON, d.LAT])[1];
             })
             .attr("r", 2)
             .on("click", function(d) {
-                    if (event.ctrlKey) {
-                        d3.select(this).remove();
-                    } else {
-                        window.open(d.URL);
-                    }
+                if (event.ctrlKey) {
+                    d3.select(this).remove();
+                    updateScores();
+                } else {
+                    window.open(d.URL);
+                }
             });
+
+        //Get 10 highest scores for table
+        csv.sort(byScore);
+        var top10 = csv.slice(0, 10);
+
+        //Create table
+        var tr = d3.select("tbody")
+            .selectAll("tr")
+            .data(top10)
+            .enter()
+            .append("tr");
+
+        tr.append("td")
+            .text(function(d) { return d.NAME; });
+
+        tr.append('td')
+            .text(function(d) { return d.SCORE; });
     });
 });
 
@@ -88,6 +108,16 @@ function convertValue(value) {
     }
 }
 
+//Sort the csv data by decreasing score
+function byScore(a, b) {
+
+    if (a.SCORE < b.SCORE)
+        return 1;
+    if (a.SCORE > b.SCORE)
+        return -1;
+    return 0;
+}
+
 function updateScores() {
 
     //Get variables from Slider Controls
@@ -98,7 +128,7 @@ function updateScores() {
     //Set query URL
     url = mongodb + wbid + "&" + ag + "&" + bio;
 
-    //console.log(url);
+    console.log(url);
 
     //Load data from MongoDB
     d3.json(url, function(json) {
@@ -126,15 +156,46 @@ function updateScores() {
                     }
                 }
             }
+
+            //Update the map
             map.selectAll("circle")
                 .data(csv)
                 .transition()
                 .attr("r", function(d) {
                     return Math.sqrt(d.SCORE);
                 });
+
+            //Get 10 highest scores for table
+            csv.sort(byScore);
+            var top10 = csv.slice(0, 10);
+
+            //Update the table
+            var old_tbody = document.getElementById('table');
+            var new_tbody = document.createElement('tbody');
+            var tr = d3.select(new_tbody)
+            .attr('id', 'table')
+            .selectAll("tr")
+            .data(top10)
+            .enter()
+            .append("tr");
+
+            //Update name cells
+            tr.append("td")
+                .on('click', function(d) {
+                    window.open(d.URL);
+                })
+                .text(function(d) { return d.NAME; });
+
+            //Update score cells
+            tr.append('td')
+                .text(function(d) { return Math.round(d.SCORE); });
+
+            //Replace the old table with the new one
+            old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
         });
+    });
 
     //Update the gauge with the new cr value
     updateGauge(cr);
-    });
+
 }
